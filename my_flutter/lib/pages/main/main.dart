1332d8726546aab54ba11cbf1955590ae62ab3c2
tanaina <tanaina@baijiahulian.com>
2020-06-11 09:46:02 +0800

    add

--- my_flutter/lib/pages/main/main.dart
+++ my_flutter/lib/pages/main/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_flutter/pages/main/initialize_items.dart';
